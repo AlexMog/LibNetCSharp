dbeae4b497fce052c4d1b6611ca4a7f97f5ce931
AlexMog <moghra_a@epitech.eu>
2014-12-11 15:47:14 +0100

    Adding the listener

---
+++ LibNet/TcpThreadListener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace mognetwork
+{
+    class TcpThreadListener
+    {
+        private Thread thread;
+        private ListenerHandler handler;
+        private TcpSocket socket;
+
+        /// <summary>
+        /// Create the listener
+        /// </summary>
+        /// <param name="handler">The handler</param>
+        /// <param name="socket">The CONNECTED socket (need to be connected to a server)</param>
+        public TcpThreadListener(ListenerHandler handler, TcpSocket socket)
+        {
+            this.handler = handler;
+            this.socket = socket;
+            thread = new Thread(run);
+        }
+
+        public void start()
+        {
+            thread.Start();
+        }
+
+        public void wait()
+        {
+            thread.Join();
+        }
+
+        public void stop()
+        {
+            thread.Interrupt(); // THIS IS NOT GOOD. I HAVE TO DO IT AGAIN AFTER.
+        }
+
+        // Not good. But, will work.
+        public void run()
+        {
+            while (true)
+            {
+                handler.onMessageReceived(socket.receiveDatas());
+            }
+        }
+    }
+}
